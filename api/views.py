f2dccc214d0c452219da24cfb1af4ad8d9523996
Mikhail Kortunov <mikhailkortunov@yandex.ru>
2021-06-27 16:39:20 +0300

    login,registration and authentication

--- api/views.py
+++ api/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import *
 from .serializers import *
 
 class ActorList(generics.ListCreateAPIView):
     queryset = Actor.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = ActorSerializer
 
 class ActorDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Actor.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = ActorSerializer
--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')), # new
+    path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
